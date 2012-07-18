25cd3495a5a111e849dca001cd7ef3924f66c2e0
Idan Gazit <idan@gazit.me>
2012-07-18 15:17:04 +0300

    Update gist-fetching JS

--- static/js/gisted.js
+++ static/js/gisted.js
@@ -1,23 +1,44 @@
 var Gisted = (function($, undefined) {
     var gist = function(gist_id) {
-        $.getJSON('https://api.github.com/gists/' + gist_id, function(data) {
-            var description = data['description'];
-            if (description) {
-                $("#description").text(description);
-            }
-            var files = data['files'];
-            keys = Object.keys(files);
-            for (var i=0; i<keys.length; i++) {
-                var file = files[keys[i]];
-                if (file['language'] == 'Markdown') {
-                    var filediv = $('<article>')
-                        .attr('class', 'file')
-                        .attr('data-filename', file['filename']);
-                    filediv.html(marked(file['content']));
-                    $('.content').append(filediv);
+        var gistxhr = $.getJSON('/' + gist_id + '/content')
+            .done(function(data) {
+                var description = data['description'];
+                if (description) {
+                    $("#description").text(description);
+                } else {
+                    $("#description").text('');
                 }
-            }
-        });
+                var files = data['files'];
+                var keys = Object.keys(files);
+                var empty = true;
+                for (var i=0; i<keys.length; i++) {
+                    var file = files[keys[i]];
+                    if (file['language'] == 'Markdown') {
+                        empty = false;
+                        var filediv = $('<article>')
+                            .attr('class', 'file')
+                            .attr('data-filename', file['filename']);
+                        filediv.html("<h1>" + file['filename'] + "</h1>" + file['rendered']);
+                        $('.content').append(filediv);
+                    }
+                }
+                if (empty) {
+                    apologize("No Content Found");
+                }
+            })
+            .fail(function(xhr, status, error) {
+                if (xhr.status == 404) {
+                    apologize("No Content Found");
+                } else {
+                    apologize("Unable to Fetch Gist");
+                }
+            })
+            .always(function() { $("#description").removeClass("loading"); });
+    };
+    var apologize = function(errorText) {
+        $("#description").text(errorText);
+        var apology = $('<p>').attr('class', 'apology').text("Quite flummoxed. Terribly sorry.");
+        $('.content').append(apology);
     };
     return {
         gist: gist
