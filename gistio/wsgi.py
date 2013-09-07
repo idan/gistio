5b71554118622fbb357955fcafaa6a480171f2d1
Idan Gazit <idan@gazit.me>
2013-09-07 20:54:46 +0300

    Introducing... DJ STATIIIIIIIC

--- gistio/wsgi.py
+++ gistio/wsgi.py
@@ -25,7 +25,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gistio.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
