94cb5969ec88813764df93e2c10d11622613a6be
Idan Gazit <idan@gazit.me>
2013-09-08 21:26:59 +0300

    Adding logging for local development

--- gistio/settings/local.py
+++ gistio/settings/local.py
@@ -2,3 +2,62 @@ from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'py.warnings': {
+            'handlers': ['console'],
+        },
+        'gistio': {
+            'handlers': ['console'],
+        },
+        'gists': {
+            'handlers': ['console'],
+        },
+        'githubauth': {
+            'handlers': ['console'],
+        },
+        'publicsite': {
+            'handlers': ['console'],
+        },
+    }
+}
