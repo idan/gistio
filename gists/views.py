e136c8dc5139949fb6ab85bc86112a859d70868d
Idan Gazit <idan@gazit.me>
2013-09-10 01:03:42 +0300

    Add proper cache checking
    Check if we have the gist before, and if we do, whether it's expired.
    Once we've fetched the gist, check to see if the updated_at timestamp is
    newer than what we already have before rendering.

--- gists/views.py
+++ gists/views.py
@@ -1,6 +1,7 @@
 import logging
 import json
 
+
 import iso8601
 import requests
 import smartypants
@@ -9,6 +10,7 @@ import rethinkdb as rdb
 
 from django.shortcuts import render, redirect
 from django.http import Http404
+from django.utils.timezone import now as tz_now
 
 from gistio.rethink import rethinkdb_connect
 from gistio.utils import get_setting
@@ -28,20 +30,26 @@ RENDERABLE = {
 
 GITHUB_AUTH_PARAMS = get_setting('GITHUB_AUTH_PARAMS')
 
-class GistFetchError(Exception): pass
+GIST_PUBLIC_CACHE_SECONDS = get_setting('GIST_PUBLIC_CACHE_SECONDS')
 
+class GistFetchError(Exception): pass
 
 @rethinkdb_connect
 def gist(request, id):
+    now = tz_now()
     gist = rdb.table('gists').get(unicode(id)).run(request.rdbconn)
     if gist is None:
-        try:
-            user, gist = fetch_and_render(request, id)
-        except GistFetchError:
-            raise Http404()
+        # it's a new gist for us
+        user, gist = fetch_and_render_gist(request, id)
     else:
+        # we already have the gist, check to see if it's still cached
+        delta = now - gist['fetched_at']
+        if delta.seconds > GIST_PUBLIC_CACHE_SECONDS:
+            raw = fetch_gist(request, id)
+            if gist['updated_at'] != iso8601.parse_date(raw['updated_at']):
+                # gist has changed, rerender it
+                gist = render_gist(request, id, raw)
         user = rdb.table('users').get(gist['author_id']).run(request.rdbconn)
-
     return render(request, 'gist.html', {'user': user, 'gist': gist})
 
 
@@ -51,8 +59,17 @@ def usergist(request, user, id):
     return gist(request, id)
 
 
-def fetch_and_render(request, id):
-    """Fetch and render a post from the Github API"""
+def fetch_and_render_gist(request, id):
+    try:
+        raw = fetch_gist(request, id)
+    except GistFetchError:
+        raise Http404()
+    user = capture_user(request, id, raw)
+    gist = render_gist(request, id, raw)
+    return user, gist
+
+def fetch_gist(request, id):
+    """Fetch a gist from the github API"""
     req_gist = requests.get('https://api.github.com/gists/{}'.format(id),
                      params=GITHUB_AUTH_PARAMS)
     if req_gist.status_code != 200:
@@ -60,17 +77,21 @@ def fetch_and_render(request, id):
         raise GistFetchError()
 
     try:
-        raw = req_gist.json()
+        return req_gist.json()
     except ValueError:
         logger.error('Fetch {} failed: unable to decode JSON response'.format(id))
         raise GistFetchError()
 
+def capture_user(request, id, raw):
     user = {}
     for prop in ['id', 'login', 'avatar_url', 'html_url', 'type']:
         user[prop] = raw['user'][prop]
     user['fetched_at'] = rdb.now()
     rdb.table('users').insert(user, upsert=True).run(request.rdbconn)
+    return user
 
+def render_gist(request, id, raw):
+    """Render a raw gist and store it"""
     gist = {
         'id': raw['id'],
         'html_url': raw['html_url'],
@@ -78,11 +99,11 @@ def fetch_and_render(request, id):
         'description': raw['description'],
         'created_at': iso8601.parse_date(raw['created_at']),
         'updated_at': iso8601.parse_date(raw['updated_at']),
-        'author_id': user['id'],
-        'author_login': user['login'],
+        'fetched_at': rdb.now(),
+        'author_id': raw['user']['id'],
+        'author_login': raw['user']['login'],
         'files': [],
     }
-
 
     for gistfile in raw['files'].values():
         format = RENDERABLE.get(gistfile['language'], None)
@@ -116,4 +137,4 @@ def fetch_and_render(request, id):
 
 
     rdb.table('gists').insert(gist, upsert=True).run(request.rdbconn)
-    return user, gist
+    return gist
