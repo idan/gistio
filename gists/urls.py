9b56881129d5465751f6d477d4ea5adb04c67fc5
Mark Watson <mwatson@millennialmedia.com>
2014-05-06 20:14:18 -0400

    changed url matching from digits to word characters [A-Za-z0-9_] to accomidate new gist id's

--- gists/urls.py
+++ gists/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^(?P<id>\d+)$', 'gists.views.gist', name='gist'),
-    url(r'^(?P<user>[a-zA-Z0-9]+)/(?P<id>\d+)$', 'gists.views.usergist', name='gist'),
+    url(r'^(?P<id>[a-zA-Z0-9]+)$', 'gists.views.gist', name='gist'),
+    url(r'^(?P<user>[a-zA-Z0-9]+)/(?P<id>[a-zA-Z0-9]+)$', 'gists.views.usergist', name='gist'),
 )
