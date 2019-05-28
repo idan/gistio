a53dff8b3f234bc34667ba17bdbf581a846f22ff
Idan Gazit <idan@gazit.me>
2019-05-28 14:35:19 -0700

    Fix homepage instructions

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,12 +9,14 @@ export default function Home() {
 
     <h1 className='f-5-ns f1 mt5 mt6-ns noligs'>Gist.io <span className='fw3 i f2-ns f3 moon-gray'>is</span><br/>Writing for Hackers</h1>
     <p>There’s a scale of permanence to writing on the web. On one end, we have the tweet: brief and ephemeral. On the other end of the scale, we have longform blog writing: unlimited in length and hopefully impervious to the passage of time.</p>
-    <p>Sometimes, we just want to share a bit of writing that is neither. Maybe we want to write for a specific audience, but don’t want to address the people who usually read our blogs. Maybe it’s just something that doesn’t fit into 140 characters.</p>
+    <p>Sometimes, we want to share a bit of writing that is neither. Maybe we want to write for a specific audience, but don’t want to address the people who usually read our blogs. Maybe it’s just something that doesn’t fit into 140 characters.</p>
     <p>Gist.io is a solution for that, inspired by Mike Bostock’s delightful <a href="https://bl.ocks.org" target="_blank">bl.ocks.org</a></p>
     <h3>Usage</h3>
     <ol>
       <li><a href="https://gist.github.com">Create a gist</a> on GitHub with a single <a href="https://commonmark.org/help/">Markdown</a>-syntax file.</li>
-      <li>In the URL bar, replace <code>gist.github.com</code> with <code>gist.io</code>.</li>
+      <li>In the URL bar, replace<br/>
+        <code><strong>gist.github.com/youruser</strong>/abc123…</code> with<br/>
+        <code><strong>gist.io/@youruser</strong>/abc123…</code>.</li>
       <li>Enjoy your beautifully-presented writing.</li>
     </ol>
 
